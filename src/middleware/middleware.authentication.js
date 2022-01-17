f0cdaf27b3b4fdbcbee7d1f7bae3d22eec4849cd
Ze-Victor <josevictorjose8@gmail.com>
2022-01-17 09:06:36 -0300

    removeUsername

--- src/middleware/middleware.authentication.js
+++ src/middleware/middleware.authentication.js
@@ -1,20 +1,19 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 function authentication(request, response, next) {
-
   const authHeader = request.headers.authorization;
 
   if (!authHeader) {
     return response.sendStatus(401);
   }
 
-  const [, token] = authHeader.split(' ');
+  const [, token] = authHeader.split(" ");
 
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, paylod) => {
-    if (err) return response.sendStatus(403)
-    request.user = paylod;
+    if (err) return response.sendStatus(403);
+    request.email = paylod;
     next();
-  })
+  });
 }
 
 module.exports = authentication;
--- src/middleware/middleware.user.js
+++ src/middleware/middleware.user.js
@@ -1,22 +1,23 @@
-const Ajv = require('ajv')
-const ajv = new Ajv()
-const addFormats = require("ajv-formats")
-const userSchema = require('../schema/schema.user')
+const Ajv = require("ajv");
+const ajv = new Ajv();
+const addFormats = require("ajv-formats");
+const userSchema = require("../schema/schema.user");
 
-addFormats(ajv)
+addFormats(ajv);
 
 function validarUsuario(request, response, next) {
+  const user = request.body;
 
-  const user = request.body
-
-  const validate = ajv.compile(userSchema)
-  const valid = validate(user)
+  const validate = ajv.compile(userSchema);
+  const valid = validate(user);
 
   if (valid) {
-    next()
+    next();
   } else {
-    response.status(400).json({ msg: "Dados inválidos", erros: validate.errors })
+    response
+      .status(400)
+      .json({ msg: "Dados inválidos", erros: validate.errors });
   }
 }
 
-module.exports = validarUsuario
+module.exports = validarUsuario;
