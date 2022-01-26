efea0d7e6f8629c5f9c8885f9b02d3f13f1d2eb4
Ze-Victor <josevictorjose8@gmail.com>
2022-01-26 11:51:12 -0300

    Alteracoes pre-integracao

--- src/index.js
+++ src/index.js
@@ -1,21 +1,29 @@
-const express = require('express')
-const routesUser = require('./routes/routes.user')
-const routesLink = require('./routes/routes.link')
-const routesCategory = require('./routes/routes.category')
-const cors = require('cors')
-require('dotenv').config()
+const express = require("express");
+const routesUser = require("./routes/routes.user");
+const routesLink = require("./routes/routes.link");
+const routesCategory = require("./routes/routes.category");
+const cors = require("cors");
+const swaggerUi = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDocument = YAML.load("./api.yaml");
+const helmet = require("helmet");
 
-const PORT = process.env.PORT || 3333
+require("dotenv").config();
 
-const app = express()
+const app = express();
 
-app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
-app.use(routesUser)
-app.use(routesCategory)
-app.use(routesLink)
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use(helmet());
+
+app.use(routesUser);
+app.use(routesCategory);
+app.use(routesLink);
+
+const PORT = process.env.PORT || 3333;
 app.listen(PORT, () => {
-  console.log('Server Start...')
-})
+  console.log("Server Start...");
+});
