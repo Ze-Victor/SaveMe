77e8af3a89e9d4d9d369148e787abd69d4c4f7b4
Ze-Victor <josevictorjose8@gmail.com>
2022-01-28 13:43:04 -0300

    Knex Migrate

--- knexfile.js
+++ knexfile.js
@@ -16,7 +16,14 @@ module.exports = {
   },
   production: {
     client: "pg",
-    connection: process.env.DATABASE_URL,
+    connection: {
+      host: "ec2-3-216-113-109.compute-1.amazonaws.com",
+      port: 5432,
+      user: "vxuzbrrlvkfdbk",
+      password:
+        "abb8630ab5cb0cce43d6ad0f2789d02637c89386b4f08ca7208de133f2df2868",
+      database: "d6cd1kl0eqardl",
+    },
     migrations: {
       directory: path.resolve(__dirname, "src", "database", "migrations"),
     },
