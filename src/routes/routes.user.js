c58a2cc9609738266fb95dd70ceb696a73209937
Ze-Victor <josevictorjose8@gmail.com>
2022-01-28 09:14:32 -0300

    Rota /

--- src/routes/routes.user.js
+++ src/routes/routes.user.js
@@ -1,18 +1,21 @@
-const express = require('express')
-const UserControllers = require('../controllers/controllers.user')
-const midUser = require('../middleware/middleware.user')
-const router = express.Router()
+const express = require("express");
+const { json, send } = require("express/lib/response");
+const UserControllers = require("../controllers/controllers.user");
+const midUser = require("../middleware/middleware.user");
+const router = express.Router();
 
-router.post('/user', midUser)
-router.put('/user/:id', midUser)
+router.post("/user", midUser);
+router.put("/user/:id", midUser);
 
-const userControllers = new UserControllers
+const userControllers = new UserControllers();
 
-router.get('/user', userControllers.index)
-router.post('/user', userControllers.create)
-router.post('/user/login', userControllers.login)
-router.put('/user/:id', userControllers.update)
-router.delete('/user/:id', userControllers.delete)
+router.get("/", (req, res) => {
+  return res.send("Hello World!");
+});
+router.get("/user", userControllers.index);
+router.post("/user", userControllers.create);
+router.post("/user/login", userControllers.login);
+router.put("/user/:id", userControllers.update);
+router.delete("/user/:id", userControllers.delete);
 
-
-module.exports = router
+module.exports = router;
