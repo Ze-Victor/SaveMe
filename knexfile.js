1e3db603a1c4dff94b9f8898967002f19ac1ea63
Ze-Victor <josevictorjose8@gmail.com>
2022-01-28 12:49:31 -0300

    Knexfile modificated

--- knexfile.js
+++ knexfile.js
@@ -1,15 +1,22 @@
 const path = require("path");
 
 module.exports = {
-  client: "pg",
-  connection: {
-    host: "localhost",
-    port: 5432,
-    user: "root",
-    password: "root",
-    database: "saveme",
+  development: {
+    client: "pg",
+    connection: {
+      host: "localhost",
+      port: 5432,
+      user: "root",
+      password: "root",
+      database: "saveme",
+    },
+    migrations: {
+      directory: path.resolve(__dirname, "src", "database", "migrations"),
+    },
   },
-  migrations: {
+  production: {
+    client: "pg",
+    connection: process.env.DATABASE_URL,
     directory: path.resolve(__dirname, "src", "database", "migrations"),
   },
 };
