2e9ba65dc4e411beccf6d7cb19449c1b39738ec3
Ze-Victor <josevictorjose8@gmail.com>
2022-01-28 08:55:11 -0300

    .

--- knexfile.js
+++ knexfile.js
@@ -3,11 +3,11 @@ const path = require("path");
 module.exports = {
   client: "pg",
   connection: {
-    host: "ec2-3-227-15-75.compute-1.amazonaws.com",
+    host: "localhost",
     port: 5432,
-    user: "rfjqkoijvwklbr",
-    password: "39ae9e6271006294994c24efad734c7602c27f83c98ffb2da7b76e52ae2f0626",
-    database: "d36gevr7ll4b8o",
+    user: "root",
+    password: "root",
+    database: "saveme",
   },
   migrations: {
     directory: path.resolve(__dirname, "src", "database", "migrations"),
